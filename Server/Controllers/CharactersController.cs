afe303962be8d5eb2c34f6fe92e7e1d3f280ca5f
cjalvaradoh <cjalvarado@gmail.com>
2024-09-22 01:35:50 -0600

    APIRYM

---
+++ Server/Controllers/CharactersController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TallerConsumoAPIRM.Shared.Models;
+
+namespace TallerConsumoAPIRM.Server.Controllers
+{
+    [Route("api/characters")]
+    public class CharactersController : Controller
+    {
+        private HttpClient _httpClient;
+        public CharactersController()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        [HttpGet]
+        [Route("all")]
+
+        public async Task<Characters> GetAllCharacters(int page)
+        {
+            try
+            {
+                Characters characters = null;
+                HttpResponseMessage httpResponseMessage = await _httpClient.GetAsync($"https://rickandmortyapi.com/api/character?page={page}");
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                string responseBody = await httpResponseMessage.Content.ReadAsStringAsync();
+
+                characters = JsonConvert.DeserializeObject<Characters>(responseBody);
+
+                return characters;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
